3ec228dd39e406e4f180577f9511c4d671168423
dlagoa <pg31552@alunos.uminho.pt>
2019-09-30 18:23:19 +0100

    queries to hibernate

--- src/main/java/pt/uminho/ceb/biosystems/merlin/sampler/AnnotationEnzymesParametersSetting.java
+++ src/main/java/pt/uminho/ceb/biosystems/merlin/sampler/AnnotationEnzymesParametersSetting.java
@@ -209,8 +209,9 @@ public class AnnotationEnzymesParametersSetting {
 	
 	/**
 	 * @return
+	 * @throws Exception 
 	 */
-	private boolean resetScorer(String blastDatabase) {
+	private boolean resetScorer(String blastDatabase) throws Exception {
 
 		int i;
 
@@ -235,9 +236,10 @@ public class AnnotationEnzymesParametersSetting {
 			case 0:
 			{
 				if(blastDatabase.equals(""))
-					HomologyAPI.resetAllScorers(statement);
+					AnnotationEnzymesServices.resetAllScorers(this.workspace.getName());
 				else
-					HomologyAPI.resetDatabaseScorer(statement, blastDatabase);
+					AnnotationEnzymesServices.resetDatabaseScorer(this.workspace.getName(), blastDatabase);
+
 				return true;
 			}
 			default:
